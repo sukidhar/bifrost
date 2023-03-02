d980a39336dbfd6fe46821ddbd9f95857d2d80bf
sukidhar <sukidhar@gmail.com>
2023-03-03 00:24:24 +0530

    add docs

--- Sources/Bifrost/Bifrost.swift
+++ Sources/Bifrost/Bifrost.swift
@@ -1,15 +1,24 @@
 import Foundation
 import Combine
 
+/// Bifrost is inspired by the term from `Thor movie` or `Norse mythology` to connect two different places.
+/// It is quite possible to get lost with plenty of errors or produce lots of repeating code across multiple projects.
+/// Bifrost aims to be the generic simple reusable module to interact with your apis. It is nowhere close to Alamofire
+/// and only goal of Bifrost is to reuse some basic networking layer across projects, with ease.
 public class Bifrost {
-    
+    /// shared instance of Bifrost.
     public static let shared = Bifrost()
     
     private init() {}
     
-    
+    /// processing background queue for URLSession
     let backgroundQueue = DispatchQueue(label: "com.suki.bifrost", qos: .default, attributes: .concurrent)
     
+    
+    /// Connects to given endpoint and process the http request with all the given configuration to `Endpoint` instance.
+    /// Oppurtunity to utilise the response of the request in the form of generic combine publisher.
+    /// - Parameter endpoint: `Endpoint` instance
+    /// - Returns: `AnyPublisher` with the provided generic object type.
     public func connect<T: Decodable>(_ endpoint: Endpoint) -> AnyPublisher<T, BiFrostError> {
         guard let request = try? endpoint.request else {
             return Fail(error: BiFrostError.invalidURLRequest).eraseToAnyPublisher()
@@ -36,6 +45,10 @@ public class Bifrost {
             .eraseToAnyPublisher()
     }
     
+    /// Connects to given endpoint and process the http request with all the given configuration to `Endpoint` instance.
+    /// Oppurtunity to utilise the response of the request in the form of latest concurrency model.
+    /// - Parameter endpoint: `Endpoint` instance.
+    /// - Returns: `T` where T is conforming to `Decodable` protocol.
     public func connect<T: Decodable>(_ endpoint: Endpoint) async throws -> T {
         let (data, response) = try await URLSession.shared.data(for: try endpoint.request)
         try response.parse()
@@ -49,6 +62,11 @@ public class Bifrost {
         }
     }
     
+    /// Connects to given endpoint and process the http request with all the given configuration to `Endpoint` instance.
+    /// Oppurtunity to utilise the response of the request in the form of latest concurrency model but with no data decoding.
+    /// It is helpful in order to download image or file content whose data should be used `Raw`.
+    /// - Parameter endpoint: `Endpoint` instance
+    /// - Returns: `Data`
     public func download(endpoint: Endpoint) async throws -> Data {
         let (data, response) = try await URLSession.shared.data(for: try endpoint.request)
         try response.parse()
